1524a2f30e09746ff0efdca78e27105aa9917817
Yair Altman <altmany@gmail.com>
2015-03-10 01:43:23 +0200

    Fixed minor warning reported by Paul Soderlind; fixed code indentation

--- print2array.m
+++ print2array.m
@@ -49,189 +49,190 @@
 %           EPS/TIF files (Javier Paredes)
 % 27/02/15: Display suggested workarounds to internal print() error (issue #16)
 % 28/02/15: Enable users to specify optional ghostscript options (issue #36)
+% 10/03/15: Fixed minor warning reported by Paul Soderlind; fixed code indentation
 
 function [A, bcol] = print2array(fig, res, renderer, gs_options)
-% Generate default input arguments, if needed
-if nargin < 2
-    res = 1;
-    if nargin < 1
-        fig = gcf;
-    end
-end
-% Warn if output is large
-old_mode = get(fig, 'Units');
-set(fig, 'Units', 'pixels');
-px = get(fig, 'Position');
-set(fig, 'Units', old_mode);
-npx = prod(px(3:4)*res)/1e6;
-if npx > 30
-    % 30M pixels or larger!
-    warning('MATLAB:LargeImage', 'print2array generating a %.1fM pixel image. This could be slow and might also cause memory problems.', npx);
-end
-% Retrieve the background colour
-bcol = get(fig, 'Color');
-% Set the resolution parameter
-res_str = ['-r' num2str(ceil(get(0, 'ScreenPixelsPerInch')*res))];
-% Generate temporary file name
-tmp_nam = [tempname '.tif'];
-try
-    % Ensure that the temp dir is writable (Javier Paredes 26/2/15)
-    fid = fopen(tmp_nam,'w');
-    fwrite(fid,1);
-    fclose(fid);
-    delete(tmp_nam);  % cleanup
-    isTempDirOk = true;
-catch
-    % Temp dir is not writable, so use the current folder
-    [dummy,fname,fext] = fileparts(tmp_nam); %#ok<ASGLU>
-    fpath = pwd;
-    tmp_nam = fullfile(fpath,[fname fext]);
-    isTempDirOk = false;
-end
-% Enable users to specify optional ghostscript options (issue #36)
-if nargin > 3 && ~isempty(gs_options)
-    if iscell(gs_options)
-        gs_options = sprintf(' %s',gs_options{:});
-    elseif ~ischar(gs_options)
-        error('gs_options input argument must be a string or cell-array of strings');
-    else
-        gs_options = [' ' gs_options];
-    end
-else
-    gs_options = '';
-end
-if nargin > 2 && strcmp(renderer, '-painters')
-    % Print to eps file
-    if isTempDirOk
-        tmp_eps = [tempname '.eps'];
-    else
-        tmp_eps = fullfile(fpath,[fname '.eps']);
-    end
-    print2eps(tmp_eps, fig, 0, renderer, '-loose');
-    try
-        % Initialize the command to export to tiff using ghostscript
-        cmd_str = ['-dEPSCrop -q -dNOPAUSE -dBATCH ' res_str ' -sDEVICE=tiff24nc'];
-        % Set the font path
-        fp = font_path();
-        if ~isempty(fp)
-            cmd_str = [cmd_str ' -sFONTPATH="' fp '"'];
+    % Generate default input arguments, if needed
+    if nargin < 2
+        res = 1;
+        if nargin < 1
+            fig = gcf;
         end
-        % Add the filenames
-        cmd_str = [cmd_str ' -sOutputFile="' tmp_nam '" "' tmp_eps '"' gs_options];
-        % Execute the ghostscript command
-        ghostscript(cmd_str);
-    catch me
+    end
+    % Warn if output is large
+    old_mode = get(fig, 'Units');
+    set(fig, 'Units', 'pixels');
+    px = get(fig, 'Position');
+    set(fig, 'Units', old_mode);
+    npx = prod(px(3:4)*res)/1e6;
+    if npx > 30
+        % 30M pixels or larger!
+        warning('MATLAB:LargeImage', 'print2array generating a %.1fM pixel image. This could be slow and might also cause memory problems.', npx);
+    end
+    % Retrieve the background colour
+    bcol = get(fig, 'Color');
+    % Set the resolution parameter
+    res_str = ['-r' num2str(ceil(get(0, 'ScreenPixelsPerInch')*res))];
+    % Generate temporary file name
+    tmp_nam = [tempname '.tif'];
+    try
+        % Ensure that the temp dir is writable (Javier Paredes 26/2/15)
+        fid = fopen(tmp_nam,'w');
+        fwrite(fid,1);
+        fclose(fid);
+        delete(tmp_nam);  % cleanup
+        isTempDirOk = true;
+    catch
+        % Temp dir is not writable, so use the current folder
+        [dummy,fname,fext] = fileparts(tmp_nam); %#ok<ASGLU>
+        fpath = pwd;
+        tmp_nam = fullfile(fpath,[fname fext]);
+        isTempDirOk = false;
+    end
+    % Enable users to specify optional ghostscript options (issue #36)
+    if nargin > 3 && ~isempty(gs_options)
+        if iscell(gs_options)
+            gs_options = sprintf(' %s',gs_options{:});
+        elseif ~ischar(gs_options)
+            error('gs_options input argument must be a string or cell-array of strings');
+        else
+            gs_options = [' ' gs_options];
+        end
+    else
+        gs_options = '';
+    end
+    if nargin > 2 && strcmp(renderer, '-painters')
+        % Print to eps file
+        if isTempDirOk
+            tmp_eps = [tempname '.eps'];
+        else
+            tmp_eps = fullfile(fpath,[fname '.eps']);
+        end
+        print2eps(tmp_eps, fig, 0, renderer, '-loose');
+        try
+            % Initialize the command to export to tiff using ghostscript
+            cmd_str = ['-dEPSCrop -q -dNOPAUSE -dBATCH ' res_str ' -sDEVICE=tiff24nc'];
+            % Set the font path
+            fp = font_path();
+            if ~isempty(fp)
+                cmd_str = [cmd_str ' -sFONTPATH="' fp '"'];
+            end
+            % Add the filenames
+            cmd_str = [cmd_str ' -sOutputFile="' tmp_nam '" "' tmp_eps '"' gs_options];
+            % Execute the ghostscript command
+            ghostscript(cmd_str);
+        catch me
+            % Delete the intermediate file
+            delete(tmp_eps);
+            rethrow(me);
+        end
         % Delete the intermediate file
         delete(tmp_eps);
-        rethrow(me);
-    end
-    % Delete the intermediate file
-    delete(tmp_eps);
-    % Read in the generated bitmap
-    A = imread(tmp_nam);
-    % Delete the temporary bitmap file
-    delete(tmp_nam);
-    % Set border pixels to the correct colour
-    if isequal(bcol, 'none')
-        bcol = [];
-    elseif isequal(bcol, [1 1 1])
-        bcol = uint8([255 255 255]);
-    else
-        for l = 1:size(A, 2)
-            if ~all(reshape(A(:,l,:) == 255, [], 1))
-                break;
-            end
-        end
-        for r = size(A, 2):-1:l
-            if ~all(reshape(A(:,r,:) == 255, [], 1))
-                break;
-            end
-        end
-        for t = 1:size(A, 1)
-            if ~all(reshape(A(t,:,:) == 255, [], 1))
-                break;
-            end
-        end
-        for b = size(A, 1):-1:t
-            if ~all(reshape(A(b,:,:) == 255, [], 1))
-                break;
-            end
-        end
-        bcol = uint8(median(single([reshape(A(:,[l r],:), [], size(A, 3)); reshape(A([t b],:,:), [], size(A, 3))]), 1));
-        for c = 1:size(A, 3)
-            A(:,[1:l-1, r+1:end],c) = bcol(c);
-            A([1:t-1, b+1:end],:,c) = bcol(c);
-        end
-    end
-else
-    if nargin < 3
-        renderer = '-opengl';
-    end
-    err = false;
-    % Set paper size
-    old_pos_mode = get(fig, 'PaperPositionMode');
-    old_orientation = get(fig, 'PaperOrientation');
-    set(fig, 'PaperPositionMode', 'auto', 'PaperOrientation', 'portrait');
-    try
-        % Print to tiff file
-        print(fig, renderer, res_str, '-dtiff', tmp_nam);
-        % Read in the printed file
+        % Read in the generated bitmap
         A = imread(tmp_nam);
-        % Delete the temporary file
+        % Delete the temporary bitmap file
         delete(tmp_nam);
-    catch ex
-        err = true;
-    end
-    % Reset paper size
-    set(fig, 'PaperPositionMode', old_pos_mode, 'PaperOrientation', old_orientation);
-    % Throw any error that occurred
-    if err
-        % Display suggested workarounds to internal print() error (issue #16)
-        fprintf(2, 'An error occured with Matlab''s builtin print function.\nTry setting the figure Renderer to ''painters'' or use opengl(''software'').\n\n');
-        rethrow(ex);
-    end
-    % Set the background color
-    if isequal(bcol, 'none')
-        bcol = [];
-    else
-        bcol = bcol * 255;
-        if isequal(bcol, round(bcol))
-            bcol = uint8(bcol);
+        % Set border pixels to the correct colour
+        if isequal(bcol, 'none')
+            bcol = [];
+        elseif isequal(bcol, [1 1 1])
+            bcol = uint8([255 255 255]);
         else
-            bcol = squeeze(A(1,1,:));
+            for l = 1:size(A, 2)
+                if ~all(reshape(A(:,l,:) == 255, [], 1))
+                    break;
+                end
+            end
+            for r = size(A, 2):-1:l
+                if ~all(reshape(A(:,r,:) == 255, [], 1))
+                    break;
+                end
+            end
+            for t = 1:size(A, 1)
+                if ~all(reshape(A(t,:,:) == 255, [], 1))
+                    break;
+                end
+            end
+            for b = size(A, 1):-1:t
+                if ~all(reshape(A(b,:,:) == 255, [], 1))
+                    break;
+                end
+            end
+            bcol = uint8(median(single([reshape(A(:,[l r],:), [], size(A, 3)); reshape(A([t b],:,:), [], size(A, 3))]), 1));
+            for c = 1:size(A, 3)
+                A(:,[1:l-1, r+1:end],c) = bcol(c);
+                A([1:t-1, b+1:end],:,c) = bcol(c);
+            end
+        end
+    else
+        if nargin < 3
+            renderer = '-opengl';
+        end
+        err = false;
+        % Set paper size
+        old_pos_mode = get(fig, 'PaperPositionMode');
+        old_orientation = get(fig, 'PaperOrientation');
+        set(fig, 'PaperPositionMode', 'auto', 'PaperOrientation', 'portrait');
+        try
+            % Print to tiff file
+            print(fig, renderer, res_str, '-dtiff', tmp_nam);
+            % Read in the printed file
+            A = imread(tmp_nam);
+            % Delete the temporary file
+            delete(tmp_nam);
+        catch ex
+            err = true;
+        end
+        % Reset paper size
+        set(fig, 'PaperPositionMode', old_pos_mode, 'PaperOrientation', old_orientation);
+        % Throw any error that occurred
+        if err
+            % Display suggested workarounds to internal print() error (issue #16)
+            fprintf(2, 'An error occured with Matlab''s builtin print function.\nTry setting the figure Renderer to ''painters'' or use opengl(''software'').\n\n');
+            rethrow(ex);
+        end
+        % Set the background color
+        if isequal(bcol, 'none')
+            bcol = [];
+        else
+            bcol = bcol * 255;
+            if isequal(bcol, round(bcol))
+                bcol = uint8(bcol);
+            else
+                bcol = squeeze(A(1,1,:));
+            end
         end
     end
-end
-% Check the output size is correct
-if isequal(res, round(res))
-    px = [px([4 3])*res 3];
-    if ~isequal(size(A), px)
-        % Correct the output size
-        A = A(1:min(end,px(1)),1:min(end,px(2)),:);
+    % Check the output size is correct
+    if isequal(res, round(res))
+        px = round([px([4 3])*res 3]);  % round() to avoid an indexing warning below
+        if ~isequal(size(A), px)
+            % Correct the output size
+            A = A(1:min(end,px(1)),1:min(end,px(2)),:);
+        end
     end
-end
 end
 
 % Function to return (and create, where necessary) the font path
 function fp = font_path()
-fp = user_string('gs_font_path');
-if ~isempty(fp)
-    return
-end
-% Create the path
-% Start with the default path
-fp = getenv('GS_FONTPATH');
-% Add on the typical directories for a given OS
-if ispc
+    fp = user_string('gs_font_path');
     if ~isempty(fp)
-        fp = [fp ';'];
+        return
     end
-    fp = [fp getenv('WINDIR') filesep 'Fonts'];
-else
-    if ~isempty(fp)
-        fp = [fp ':'];
+    % Create the path
+    % Start with the default path
+    fp = getenv('GS_FONTPATH');
+    % Add on the typical directories for a given OS
+    if ispc
+        if ~isempty(fp)
+            fp = [fp ';'];
+        end
+        fp = [fp getenv('WINDIR') filesep 'Fonts'];
+    else
+        if ~isempty(fp)
+            fp = [fp ':'];
+        end
+        fp = [fp '/usr/share/fonts:/usr/local/share/fonts:/usr/share/fonts/X11:/usr/local/share/fonts/X11:/usr/share/fonts/truetype:/usr/local/share/fonts/truetype'];
     end
-    fp = [fp '/usr/share/fonts:/usr/local/share/fonts:/usr/share/fonts/X11:/usr/local/share/fonts/X11:/usr/share/fonts/truetype:/usr/local/share/fonts/truetype'];
-end
-user_string('gs_font_path', fp);
+    user_string('gs_font_path', fp);
 end
