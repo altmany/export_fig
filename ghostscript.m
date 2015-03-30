88a73a8afaf925d904e3e3c19e22090a82023e79
Yair Altman <altmany@gmail.com>
2015-03-30 14:12:40 +0300

    Improved performance by caching status of GS path check, if ok

--- ghostscript.m
+++ ghostscript.m
@@ -38,6 +38,7 @@
 % 23/01/14 - Add full path to ghostscript.txt in warning. Thanks to Koen
 %            Vermeer for raising the issue.
 % 27/02/15 - If Ghostscript croaks, display suggested workarounds
+% 30/03/15 - Improved performance by caching status of GS path check, if ok
 
 function varargout = ghostscript(cmd)
     try
@@ -66,116 +67,120 @@ function varargout = ghostscript(cmd)
 end
 
 function path_ = gs_path
-% Return a valid path
-% Start with the currently set path
-path_ = user_string('ghostscript');
-% Check the path works
-if check_gs_path(path_)
-    return
-end
-% Check whether the binary is on the path
-if ispc
-    bin = {'gswin32c.exe', 'gswin64c.exe', 'gs'};
-else
-    bin = {'gs'};
-end
-for a = 1:numel(bin)
-    path_ = bin{a};
-    if check_store_gs_path(path_)
+    % Return a valid path
+    % Start with the currently set path
+    path_ = user_string('ghostscript');
+    % Check the path works
+    if check_gs_path(path_)
         return
     end
-end
-% Search the obvious places
-if ispc
-    default_location = 'C:\Program Files\gs\';
-    dir_list = dir(default_location);
-    if isempty(dir_list)
-        default_location = 'C:\Program Files (x86)\gs\'; % Possible location on 64-bit systems 
-        dir_list = dir(default_location);
+    % Check whether the binary is on the path
+    if ispc
+        bin = {'gswin32c.exe', 'gswin64c.exe', 'gs'};
+    else
+        bin = {'gs'};
     end
-    executable = {'\bin\gswin32c.exe', '\bin\gswin64c.exe'};
-    ver_num = 0;
-    % If there are multiple versions, use the newest
-    for a = 1:numel(dir_list)
-        ver_num2 = sscanf(dir_list(a).name, 'gs%g');
-        if ~isempty(ver_num2) && ver_num2 > ver_num
-            for b = 1:numel(executable)
-                path2 = [default_location dir_list(a).name executable{b}];
-                if exist(path2, 'file') == 2
-                    path_ = path2;
-                    ver_num = ver_num2;
-                end
-            end
-        end
-    end
-    if check_store_gs_path(path_)
-        return
-    end
-else
-    executable = {'/usr/bin/gs', '/usr/local/bin/gs'};
-    for a = 1:numel(executable)
-        path_ = executable{a};
+    for a = 1:numel(bin)
+        path_ = bin{a};
         if check_store_gs_path(path_)
             return
         end
     end
-end
-% Ask the user to enter the path
-while 1
-    if strncmp(computer, 'MAC', 3) % Is a Mac
-        % Give separate warning as the uigetdir dialogue box doesn't have a
-        % title
-        uiwait(warndlg('Ghostscript not found. Please locate the program.'))
+    % Search the obvious places
+    if ispc
+        default_location = 'C:\Program Files\gs\';
+        dir_list = dir(default_location);
+        if isempty(dir_list)
+            default_location = 'C:\Program Files (x86)\gs\'; % Possible location on 64-bit systems
+            dir_list = dir(default_location);
+        end
+        executable = {'\bin\gswin32c.exe', '\bin\gswin64c.exe'};
+        ver_num = 0;
+        % If there are multiple versions, use the newest
+        for a = 1:numel(dir_list)
+            ver_num2 = sscanf(dir_list(a).name, 'gs%g');
+            if ~isempty(ver_num2) && ver_num2 > ver_num
+                for b = 1:numel(executable)
+                    path2 = [default_location dir_list(a).name executable{b}];
+                    if exist(path2, 'file') == 2
+                        path_ = path2;
+                        ver_num = ver_num2;
+                    end
+                end
+            end
+        end
+        if check_store_gs_path(path_)
+            return
+        end
+    else
+        executable = {'/usr/bin/gs', '/usr/local/bin/gs'};
+        for a = 1:numel(executable)
+            path_ = executable{a};
+            if check_store_gs_path(path_)
+                return
+            end
+        end
     end
-    base = uigetdir('/', 'Ghostcript not found. Please locate the program.');
-    if isequal(base, 0)
-        % User hit cancel or closed window
-        break;
-    end
-    base = [base filesep];
-    bin_dir = {'', ['bin' filesep], ['lib' filesep]};
-    for a = 1:numel(bin_dir)
-        for b = 1:numel(bin)
-            path_ = [base bin_dir{a} bin{b}];
-            if exist(path_, 'file') == 2
-                if check_store_gs_path(path_)
-                    return
+    % Ask the user to enter the path
+    while true
+        if strncmp(computer, 'MAC', 3) % Is a Mac
+            % Give separate warning as the uigetdir dialogue box doesn't have a
+            % title
+            uiwait(warndlg('Ghostscript not found. Please locate the program.'))
+        end
+        base = uigetdir('/', 'Ghostcript not found. Please locate the program.');
+        if isequal(base, 0)
+            % User hit cancel or closed window
+            break;
+        end
+        base = [base filesep];
+        bin_dir = {'', ['bin' filesep], ['lib' filesep]};
+        for a = 1:numel(bin_dir)
+            for b = 1:numel(bin)
+                path_ = [base bin_dir{a} bin{b}];
+                if exist(path_, 'file') == 2
+                    if check_store_gs_path(path_)
+                        return
+                    end
                 end
             end
         end
     end
-end
-error('Ghostscript not found. Have you installed it from www.ghostscript.com?');
+    error('Ghostscript not found. Have you installed it from www.ghostscript.com?');
 end
 
 function good = check_store_gs_path(path_)
-% Check the path is valid
-good = check_gs_path(path_);
-if ~good
-    return
-end
-% Update the current default path to the path found
-if ~user_string('ghostscript', path_)
-    warning('Path to ghostscript installation could not be saved. Enter it manually in %s.', fullfile(fileparts(which('user_string.m')), '.ignore', 'ghostscript.txt'));
-    return
-end
+    % Check the path is valid
+    good = check_gs_path(path_);
+    if ~good
+        return
+    end
+    % Update the current default path to the path found
+    if ~user_string('ghostscript', path_)
+        warning('Path to ghostscript installation could not be saved. Enter it manually in %s.', fullfile(fileparts(which('user_string.m')), '.ignore', 'ghostscript.txt'));
+        return
+    end
 end
 
 function good = check_gs_path(path_)
-% Check the path is valid
-[good, message] = system([gs_command(path_) '-h']);
-good = good == 0;
+    persistent isOk
+    if ~isequal(isOk,true)
+        % Check whether the path is valid
+        [status, message] = system([gs_command(path_) '-h']);
+        isOk = status == 0;
+    end
+    good = isOk;
 end
 
 function cmd = gs_command(path_)
-% Initialize any required system calls before calling ghostscript
-shell_cmd = '';
-if isunix
-    shell_cmd = 'export LD_LIBRARY_PATH=""; '; % Avoids an error on Linux with GS 9.07
-end
-if ismac
-    shell_cmd = 'export DYLD_LIBRARY_PATH=""; ';  % Avoids an error on Mac with GS 9.07
-end
-% Construct the command string
-cmd = sprintf('%s"%s" ', shell_cmd, path_);
+    % Initialize any required system calls before calling ghostscript
+    shell_cmd = '';
+    if isunix
+        shell_cmd = 'export LD_LIBRARY_PATH=""; '; % Avoids an error on Linux with GS 9.07
+    end
+    if ismac
+        shell_cmd = 'export DYLD_LIBRARY_PATH=""; ';  % Avoids an error on Mac with GS 9.07
+    end
+    % Construct the command string
+    cmd = sprintf('%s"%s" ', shell_cmd, path_);
 end
