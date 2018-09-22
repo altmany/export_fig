e18edb5eaefc9f0351b59920e4f38860204331c5
Yair Altman <altmany@gmail.com>
2018-09-22 23:03:44 +0300

    Xpdf website changed to xpdfreader.com

--- pdf2eps.m
+++ pdf2eps.m
@@ -7,45 +7,49 @@
 %
 % This function requires that you have pdftops, from the Xpdf suite of
 % functions, installed on your system. This can be downloaded from:
-% http://www.foolabs.com/xpdf  
+% http://xpdfreader.com
 %
-%IN:
+% Inputs:
 %   source - filename of the source pdf file to convert. The filename is
 %            assumed to already have the extension ".pdf".
 %   dest - filename of the destination eps file. The filename is assumed to
 %          already have the extension ".eps".
 
-% Copyright (C) Oliver Woodford 2009-2010
+% Copyright (C) Oliver Woodford 2009-2010, Yair Altman 2015-
 
 % Thanks to Aldebaro Klautau for reporting a bug when saving to
 % non-existant directories.
 
-function pdf2eps(source, dest)
-% Construct the options string for pdftops
-options = ['-q -paper match -eps -level2 "' source '" "' dest '"'];
-% Convert to eps using pdftops
-[status, message] = pdftops(options);
-% Check for error
-if status
-    % Report error
-    if isempty(message)
-        error('Unable to generate eps. Check destination directory is writable.');
-    else
-        error(message);
-    end
-end
-% Fix the DSC error created by pdftops
-fid = fopen(dest, 'r+');
-if fid == -1
-    % Cannot open the file
-    return
-end
-fgetl(fid); % Get the first line
-str = fgetl(fid); % Get the second line
-if strcmp(str(1:min(13, end)), '% Produced by')
-    fseek(fid, -numel(str)-1, 'cof');
-    fwrite(fid, '%'); % Turn ' ' into '%'
-end
-fclose(fid);
-end
+% 22/09/2018 - Xpdf website changed to xpdfreader.com
 
+function pdf2eps(source, dest)
+    % Construct the options string for pdftops
+    options = ['-q -paper match -eps -level2 "' source '" "' dest '"'];
+
+    % Convert to eps using pdftops
+    [status, message] = pdftops(options);
+
+    % Check for error
+    if status
+        % Report error
+        if isempty(message)
+            error('Unable to generate eps. Check destination directory is writable.');
+        else
+            error(message);
+        end
+    end
+
+    % Fix the DSC error created by pdftops
+    fid = fopen(dest, 'r+');
+    if fid == -1
+        % Cannot open the file
+        return
+    end
+    fgetl(fid); % Get the first line
+    str = fgetl(fid); % Get the second line
+    if strcmp(str(1:min(13, end)), '% Produced by')
+        fseek(fid, -numel(str)-1, 'cof');
+        fwrite(fid, '%'); % Turn ' ' into '%'
+    end
+    fclose(fid);
+end
