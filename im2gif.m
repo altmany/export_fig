09a1f86c4153c363c564a65ebf8ca164481672a2
Yair Altman <altmany@gmail.com>
2019-11-30 21:09:23 +0200

    Indentation + added comment for issue #288 merge

--- im2gif.m
+++ im2gif.m
@@ -43,162 +43,163 @@
 
 %{
 % 14/02/18: Merged issue #235: reduced memory usage, improved performance (thanks to @numb7rs)
+% 30/11/19: Merged issue #288: Fix im2gif.m for greyscale TIFF images (thanks @Blackbelt1221)
 %}
 
 function im2gif(A, varargin)
 
-% Parse the input arguments
-[A, options] = parse_args(A, varargin{:});
+    % Parse the input arguments
+    [A, options] = parse_args(A, varargin{:});
 
-if options.crop ~= 0
-    % Crop
-    A = crop_borders(A, A(ceil(end/2),1,:,1));
-end
+    if options.crop ~= 0
+        % Crop
+        A = crop_borders(A, A(ceil(end/2),1,:,1));
+    end
 
-% Convert to indexed image
-[h, w, c, n] = size(A);
+    % Convert to indexed image
+    [h, w, c, n] = size(A);
 
-% Issue #235: Using unique(A,'rows') on the whole image stack at once causes
-% massive memory usage when dealing with large images (at least on Matlab 2017b).
-% Running unique(...) on individual frames, then again on the results drastically
-% reduces the memory usage & slightly improves the execution time (@numb7rs).
-uns = cell(1,size(A,4));
-for nn=1:size(A,4)
-    uns{nn}=unique(reshape(A(:,:,:,nn), h*w, c),'rows');
-end
-map=unique(cell2mat(uns'),'rows');
+    % Issue #235: Using unique(A,'rows') on the whole image stack at once causes
+    % massive memory usage when dealing with large images (at least on Matlab 2017b).
+    % Running unique(...) on individual frames, then again on the results drastically
+    % reduces the memory usage & slightly improves the execution time (@numb7rs).
+    uns = cell(1,size(A,4));
+    for nn=1:size(A,4)
+        uns{nn}=unique(reshape(A(:,:,:,nn), h*w, c),'rows');
+    end
+    map=unique(cell2mat(uns'),'rows');
 
-A = reshape(permute(A, [1 2 4 3]), h, w*n, c);
+    A = reshape(permute(A, [1 2 4 3]), h, w*n, c);
 
-if size(map, 1) > 256
-    dither_str = {'dither', 'nodither'};
-    dither_str = dither_str{1+(options.dither==0)};
-    if options.ncolors <= 1
-        [B, map] = rgb2ind(A, options.ncolors, dither_str);
-        if size(map, 1) > 256
-            [B, map] = rgb2ind(A, 256, dither_str);
+    if size(map, 1) > 256
+        dither_str = {'dither', 'nodither'};
+        dither_str = dither_str{1+(options.dither==0)};
+        if options.ncolors <= 1
+            [B, map] = rgb2ind(A, options.ncolors, dither_str);
+            if size(map, 1) > 256
+                [B, map] = rgb2ind(A, 256, dither_str);
+            end
+        else
+            [B, map] = rgb2ind(A, min(round(options.ncolors), 256), dither_str);
         end
     else
-        [B, map] = rgb2ind(A, min(round(options.ncolors), 256), dither_str);
+        if max(map(:)) > 1
+            map = double(map) / 255;
+            A = double(A) / 255;
+        end
+        B = rgb2ind(im2double(A), map);
     end
-else
-    if max(map(:)) > 1
-        map = double(map) / 255;
-        A = double(A) / 255;
-    end
-    B = rgb2ind(im2double(A), map);
-end
-B = reshape(B, h, w, 1, n);
+    B = reshape(B, h, w, 1, n);
 
-% Bug fix to rgb2ind
-map(B(1)+1,:) = im2double(A(1,1,:));
+    % Bug fix to rgb2ind
+    map(B(1)+1,:) = im2double(A(1,1,:));
 
-% Save as a gif
-imwrite(B, map, options.outfile, 'LoopCount', round(options.loops(1)), 'DelayTime', options.delay);
+    % Save as a gif
+    imwrite(B, map, options.outfile, 'LoopCount', round(options.loops(1)), 'DelayTime', options.delay);
 end
 
 %% Parse the input arguments
 function [A, options] = parse_args(A, varargin)
-% Set the defaults
-options = struct('outfile', '', ...
-                 'dither', true, ...
-                 'crop', true, ...
-                 'ncolors', 256, ...
-                 'loops', 65535, ...
-                 'delay', 1/15);
+    % Set the defaults
+    options = struct('outfile', '', ...
+                     'dither', true, ...
+                     'crop', true, ...
+                     'ncolors', 256, ...
+                     'loops', 65535, ...
+                     'delay', 1/15);
 
-% Go through the arguments
-a = 0;
-n = numel(varargin);
-while a < n
-    a = a + 1;
-    if ischar(varargin{a}) && ~isempty(varargin{a})
-        if varargin{a}(1) == '-'
-            opt = lower(varargin{a}(2:end));
-            switch opt
-                case 'nocrop'
-                    options.crop = false;
-                case 'nodither'
-                    options.dither = false;
-                otherwise
-                    if ~isfield(options, opt)
-                        error('Option %s not recognized', varargin{a});
-                    end
-                    a = a + 1;
-                    if ischar(varargin{a}) && ~ischar(options.(opt))
-                        options.(opt) = str2double(varargin{a});
-                    else
-                        options.(opt) = varargin{a};
-                    end
+    % Go through the arguments
+    a = 0;
+    n = numel(varargin);
+    while a < n
+        a = a + 1;
+        if ischar(varargin{a}) && ~isempty(varargin{a})
+            if varargin{a}(1) == '-'
+                opt = lower(varargin{a}(2:end));
+                switch opt
+                    case 'nocrop'
+                        options.crop = false;
+                    case 'nodither'
+                        options.dither = false;
+                    otherwise
+                        if ~isfield(options, opt)
+                            error('Option %s not recognized', varargin{a});
+                        end
+                        a = a + 1;
+                        if ischar(varargin{a}) && ~ischar(options.(opt))
+                            options.(opt) = str2double(varargin{a});
+                        else
+                            options.(opt) = varargin{a};
+                        end
+                end
+            else
+                options.outfile = varargin{a};
             end
-        else
-            options.outfile = varargin{a};
         end
     end
-end
 
-if isempty(options.outfile)
-    if ~ischar(A)
-        error('No output filename given.');
+    if isempty(options.outfile)
+        if ~ischar(A)
+            error('No output filename given.');
+        end
+        % Generate the output filename from the input filename
+        [path, outfile] = fileparts(A);
+        options.outfile = fullfile(path, [outfile '.gif']);
     end
-    % Generate the output filename from the input filename
-    [path, outfile] = fileparts(A);
-    options.outfile = fullfile(path, [outfile '.gif']);
-end
 
-if ischar(A)
-    % Read in the image
-    A = imread_rgb(A);
-end
+    if ischar(A)
+        % Read in the image
+        A = imread_rgb(A);
+    end
 end
 
 %% Read image to uint8 rgb array
 function [A, alpha] = imread_rgb(name)
-% Get file info
-info = imfinfo(name);
-% Special case formats
-switch lower(info(1).Format)
-    case 'gif'
-        [A, map] = imread(name, 'frames', 'all');
-        if ~isempty(map)
-            map = uint8(map * 256 - 0.5); % Convert to uint8 for storage
-            A = reshape(map(uint32(A)+1,:), [size(A) size(map, 2)]); % Assume indexed from 0
-            A = permute(A, [1 2 5 4 3]);
-        end
-    case {'tif', 'tiff'}
-        A = cell(numel(info), 1);
-        for a = 1:numel(A)
-            [A{a}, map] = imread(name, 'Index', a, 'Info', info);
+    % Get file info
+    info = imfinfo(name);
+    % Special case formats
+    switch lower(info(1).Format)
+        case 'gif'
+            [A, map] = imread(name, 'frames', 'all');
             if ~isempty(map)
                 map = uint8(map * 256 - 0.5); % Convert to uint8 for storage
-                A{a} = reshape(map(uint32(A{a})+1,:), [size(A) size(map, 2)]); % Assume indexed from 0
+                A = reshape(map(uint32(A)+1,:), [size(A) size(map, 2)]); % Assume indexed from 0
+                A = permute(A, [1 2 5 4 3]);
             end
-            if size(A{a}, 3) == 4
-                % TIFF in CMYK colourspace - convert to RGB
-                if isfloat(A{a})
-                    A{a} = A{a} * 255;
-                else
-                    A{a} = single(A{a});
+        case {'tif', 'tiff'}
+            A = cell(numel(info), 1);
+            for a = 1:numel(A)
+                [A{a}, map] = imread(name, 'Index', a, 'Info', info);
+                if ~isempty(map)
+                    map = uint8(map * 256 - 0.5); % Convert to uint8 for storage
+                    A{a} = reshape(map(uint32(A{a})+1,:), [size(A) size(map, 2)]); % Assume indexed from 0
                 end
-                A{a} = 255 - A{a};
-                A{a}(:,:,4) = A{a}(:,:,4) / 255;
-                A{a} = uint8(A(:,:,1:3) .* A{a}(:,:,[4 4 4]));
-            elseif size(A{a}, 3) < 3 %Check whether TIFF has been read in as greyscale
-                %Convert from greyscale to RGB colorspace
-                A{a} = cat(3, A{a}, A{a}, A{a});
+                if size(A{a}, 3) == 4
+                    % TIFF in CMYK colourspace - convert to RGB
+                    if isfloat(A{a})
+                        A{a} = A{a} * 255;
+                    else
+                        A{a} = single(A{a});
+                    end
+                    A{a} = 255 - A{a};
+                    A{a}(:,:,4) = A{a}(:,:,4) / 255;
+                    A{a} = uint8(A(:,:,1:3) .* A{a}(:,:,[4 4 4]));
+                elseif size(A{a}, 3) < 3 %Check whether TIFF has been read in as greyscale
+                    %Convert from greyscale to RGB colorspace (issue #288)
+                    A{a} = cat(3, A{a}, A{a}, A{a});
+                end
             end
-        end
-        A = cat(4, A{:});
-    otherwise
-        [A, map, alpha] = imread(name);
-        A = A(:,:,:,1); % Keep only first frame of multi-frame files
-        if ~isempty(map)
-            map = uint8(map * 256 - 0.5); % Convert to uint8 for storage
-            A = reshape(map(uint32(A)+1,:), [size(A) size(map, 2)]); % Assume indexed from 0
-        elseif size(A, 3) == 4
-            % Assume 4th channel is an alpha matte
-            alpha = A(:,:,4);
-            A = A(:,:,1:3);
-        end
-end
+            A = cat(4, A{:});
+        otherwise
+            [A, map, alpha] = imread(name);
+            A = A(:,:,:,1); % Keep only first frame of multi-frame files
+            if ~isempty(map)
+                map = uint8(map * 256 - 0.5); % Convert to uint8 for storage
+                A = reshape(map(uint32(A)+1,:), [size(A) size(map, 2)]); % Assume indexed from 0
+            elseif size(A, 3) == 4
+                % Assume 4th channel is an alpha matte
+                alpha = A(:,:,4);
+                A = A(:,:,1:3);
+            end
+    end
 end
