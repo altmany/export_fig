2379e30f52ff479618f16964c8233dd7ed9ead07
Yair Altman <altmany@gmail.com>
2021-03-11 13:54:35 +0200

    Added documentation about export_options.regexprep; added sanity check (issue #324)

--- print2eps.m
+++ print2eps.m
@@ -19,20 +19,23 @@ function print2eps(name, fig, export_options, varargin)
 %              ".eps" extension is added if not there already. If a path is
 %              not specified, the figure is saved in the current directory.
 %   fig_handle - The handle of the figure to be saved. Default: gcf().
-%   export_options - array or struct of optional scalar values:
-%       bb_padding - Scalar value of amount of padding to add to border around
-%                    the cropped image, in points (if >1) or percent (if <1).
-%                    Can be negative as well as positive; Default: 0
-%       crop       - Cropping flag. Deafult: 0
-%       fontswap   - Whether to swap non-default fonts in figure. Default: true
+%   export_options - array or struct of optional values:
+%       bb_padding    - Scalar value of amount of padding to add to border around
+%                       the cropped image, in points (if >1) or percent (if <1).
+%                       Can be negative as well as positive; Default: 0
+%       crop          - Cropping flag. Deafult: 0
+%       fontswap      - Whether to swap non-default fonts in figure. Default: true
 %       preserve_size - Whether to preserve the figure's PaperSize. Default: false
-%       font_space - Character used to separate font-name terms in the EPS output
-%                    e.g. "Courier New" => "Courier-New". Default: ''
-%                    (available only via the struct alternative)
-%       renderer   - Renderer used to generate bounding-box. Default: 'opengl'
-%                    (available only via the struct alternative)
-%       crop_amounts - 4-element vector of crop amounts: [top,right,bottom,left]
-%                    (available only via the struct alternative)
+%       font_space    - Character used to separate font-name terms in the EPS output
+%                       e.g. "Courier New" => "Courier-New". Default: ''
+%                       (available only via the struct alternative)
+%       renderer      - Renderer used to generate bounding-box. Default: 'opengl'
+%                       (available only via the struct alternative)
+%       crop_amounts  - 4-element vector of crop amounts: [top,right,bottom,left]
+%                       (available only via the struct alternative)
+%       regexprep     - 2-element cell-array of regular-expression replacement in the
+%                       generated EPS. 1st element is the replaced string(s), 2nd is
+%                       the replacement(s) (available only via the struct alternative)
 %   print_options - Additional parameter strings to be passed to the print command
 
 %{
@@ -106,6 +109,7 @@ function print2eps(name, fig, export_options, varargin)
 % 15/01/20: Added warning ID for easier suppression by users
 % 20/01/20: Added comment about unsupported patch transparency in some Ghostscript versions (issue #285)
 % 10/12/20: Enabled user-specified regexp replacements in the generated EPS file (issue #324)
+% 11/03/21: Added documentation about export_options.regexprep; added sanity check (issue #324)
 %}
 
     options = {'-loose'};
@@ -569,7 +573,7 @@ function print2eps(name, fig, export_options, varargin)
     fstrm = regexprep(fstrm, '\n([-\d.]+ [-\d.]+) ([-\d.]+ [-\d.]+) ([-\d.]+ [-\d.]+) 3 MP\nPP\n\3 \2 \1 3 MP\nPP\n','\n$1 $2 $3 0 0 4 MP\nPP\n');
 
     % If user requested a regexprep replacement of string(s), do this now (issue #324)
-    if ~isempty(export_options.regexprep)
+    if isstruct(export_options) && isfield(export_options,'regexprep') %&& ~isempty(export_options.regexprep)
         try
             oldStrOrRegexp = export_options.regexprep{1};
             newStrOrRegexp = export_options.regexprep{2};
